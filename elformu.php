efc453de8666dbded08d9c612470a398aed5622a
HannahFord12 <isabelmanzana12@gmail.com>
2022-10-17 13:40:38 +0200

    Formulario de registro

--- elformu.php
+++ elformu.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     <?php
+        //ejercicio php formulario registro
         $opciones = array(
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
