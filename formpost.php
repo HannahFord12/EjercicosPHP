5732d11d5cdfffa53cfb2622091c6710b826162a
HannahFord12 <isabelmanzana12@gmail.com>
2022-10-17 13:41:44 +0200

    Formulario de ejemplo

--- formpost.php
+++ formpost.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<!--Formulario ejemplo-->
 <body>
     Hola 
     <?php echo $_POST["nombre"]; ?>
