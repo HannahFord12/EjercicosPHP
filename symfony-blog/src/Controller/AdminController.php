68e782f9644daa03f2561acac3da6e3fa8f69d60
HannahFord12 <isabelmanzana12@gmail.com>
2022-11-02 09:07:25 +0100

    Administracion

---
+++ symfony-blog/src/Controller/AdminController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CategoryFormType;
+use App\Entity\Category;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+    public function adminDashboard(): Response{
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+    
+        // or add an optional message - seen by developers
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+        
+        new Response("Sí que puedes entrar");
+    }
+    
+    /**
+     * @Route("/admin/imagenes", name="app_imagenes")
+     */
+    public function imagenes():Response{
+        return $this->render('admin/imagenes.html.twig', []);
+    }
+    /**
+    * @Route("/admin/categories", name="app_categories")
+    */
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $repositorio = $doctrine->getRepository(Category::class);
+
+        $categories = $repositorio->findAll();
+
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories   
+        ));
+
+    }
+
+
+}
