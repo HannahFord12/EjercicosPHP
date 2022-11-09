a174d90ad948b1d52ffabf5d0dc4b61309034137
HannahFord12 <isabelmanzana12@gmail.com>
2022-11-09 10:36:50 +0100

    5.4 Ruta

--- symfony-blog/src/Controller/BlogController.php
+++ symfony-blog/src/Controller/BlogController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Entity\Post;
+use App\Form\PostFormType;
 
 class BlogController extends AbstractController
 {
@@ -22,9 +27,41 @@ class BlogController extends AbstractController
         return $this->render('publi/contacto.html',[]);
     }
     /**
-     * @Route("/single_post", name="single_post")
-     */
-    public function single_post():Response{
-        return $this->render('publi/single_post.html',[]);
+    * @Route("/single_post/{slug}", name="single_post")
+    */
+    public function post(ManagerRegistry $doctrine, $slug): Response
+    {
+        $repositorio = $doctrine->getRepository(Post::class);
+        $post = $repositorio->findOneBy(["slug"=>$slug]);
+        return $this->render('blog/single_post.html.twig', [
+            'post' => $post,
+        ]);
     }
+    /**
+    * @Route("/blog/new", name="new_post")
+    */
+    public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();   
+            $post->setSlug($slugger->slug($post->getTitle()));
+            $post->setPostUser($this->getUser());
+            $post->setNumLikes(0);
+            $post->setNumComments(0);
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->render('blog/new_post.html.twig', array(
+                'form' => $form->createView()    
+            ));
+            //return $this->redirectToRoute('single_post', ["slug" => $post->getSlug()]);
+        }   //todos los que redirigen a singel post hay que ponerlos a slug
+        return $this->render('blog/new_post.html.twig', array(
+            'form' => $form->createView()    
+        ));
+    }
+
 }
---
+++ symfony-blog/src/Form/PostFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextType::class)
+            ->add('publishedAt')
+            ->add('image', FileType::class ) //tipo file
+            ->add('Send', SubmitType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
